aa79c1801345e733338a6b613374c8237e984662
Raspopova <sonyaraspopova>
2020-11-26 11:40:55 +0500

    added new line test

--- application/cells/UserTableViewCell.swift
+++ application/cells/UserTableViewCell.swift
@@ -17,6 +17,7 @@ class UserTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
